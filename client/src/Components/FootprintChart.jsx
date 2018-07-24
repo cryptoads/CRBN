d8d5b6f4b3ba740e6be49b707132cca468edad4f
Carlton Freeman <carltonfreeman@Carltons-MacBook-Pro.local>
2018-07-24 18:58:36 -0400

    implement doughnut chart in 'Footprint' section, remove 'Feed' section until we have a use for it (maybe v2?)

--- client/src/Components/FootprintChart.jsx
+++ client/src/Components/FootprintChart.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-
+import { Doughnut } from 'react-chartjs-2';
 
 class FootprintChart extends Component {
 
   render() {
-    let isLoggedIn = this.props.isLoggedIn; 
+    let isLoggedIn = this.props.isLoggedIn;
+    let data = {
+      datasets: [{
+          data: [10, 20, 30],
+          backgroundColor : [
+            '#08E6C8','#472029', '#a7ed9c' 
+          ],
+      }],
+  
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: [
+          'Vehicle',
+          'Home',
+          'Waste'
+      ]
+  };
+
     let chartJSX = (
     <div className='profile-info chart-container col-8'>
       <h2>Footprint</h2>
-        <span onClick={this.editLinkClick.bind(this)} className="edit-footprint-info">Edit Footprint Info</span>
+        <span onClick={this.editLinkClick.bind(this)} className="edit-info-link">Edit Footprint Info</span>
+        <Doughnut data={data} />
     </div>)
     return (
       chartJSX
@@ -22,6 +39,7 @@ class FootprintChart extends Component {
     modalOverlay.classList.toggle('closed');
   }
 }
+
 
 export default FootprintChart;
 
