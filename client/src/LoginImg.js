687b15c995108fc85d801862400a0a8083f5dadb
jarrodt123 <33099532+jarrodt123@users.noreply.github.com>
2018-08-04 07:20:57 -0400

    added LoginImg component

---
+++ client/src/LoginImg.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Background from './LoginImg.jpg';
+
+const LoginImg = {
+    width: '100vw',
+    height: '100vh',
+    backgroundImage: `url(${Background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    overflow: 'hidden'
+  };
+  
+  export default LoginImg;
