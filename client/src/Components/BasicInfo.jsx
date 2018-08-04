97fd9b0c044cb99c19b1dd308dd4e8fa0802e1a9
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-04 10:12:44 -0400

    fix size

--- client/src/Components/BasicInfo.jsx
+++ client/src/Components/BasicInfo.jsx
@@ -17,7 +17,7 @@ class BasicInfo extends Component
   {  
     let basicInfo = this.props.basicInfo;
     let loggedIn = this.props.loggedIn;
-    return (<div className="basic-info-container col-4">
+    return (<div className="basic-info-container col-sm-12 col-md-4 col-lg-4">
               <img alt="profile-pic" className='profile-picture' src={basicInfo.imgUrl} />
               <span onClick={this.editButtonClicked} className='edit-info-link'> { loggedIn !== false ? 'Edit Basic Info' : '' }</span>
               <button onClick={this.saveButtonClicked} className="saveButton appBtn hide">Save</button>
