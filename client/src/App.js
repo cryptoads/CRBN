4aebd215b1e05a8018df67aaeb8af7ee068cdad7
Carlton Freeman <carltonfreeman@Carltons-MBP.hsd1.ga.comcast.net>
2018-08-12 00:10:26 -0400

    basic EventsList component pieces

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-
-import axios from 'axios'
+import { Link, Route } from 'react-router-dom';
+import axios from 'axios';
 import Login from './Components/Login';
 import UserProfile from './Components/UserProfile';
-
+import EventsList from './Components/EventsList';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { loggedIn: false }
+    this.state = {loggedIn: false};
     this.logout = this.logout.bind(this);
-
   }
 
   componentWillMount() {
-    axios.get('/test')
-    .then((res)=>{
-      this.setState({loggedIn: res.data.loggedIn})
-      console.log(this.state.loggedIn)
-      console.log(res)
-    })
+    axios.get('/test').then(res => {
+      this.setState({loggedIn: res.data.loggedIn});
+      console.log(this.state.loggedIn);
+      console.log(res);
+    });
   }
-
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">CRBN</h1>
-          { this.state.loggedIn ? 
-		  <React.Fragment>
-		  <button className="appBtn" onClick={this.logout}>Log Out</button> 
-		  <button className="appBtn">Events</button> 
-		  </React.Fragment>
-		  : "" }
+          {this.state.loggedIn ? (
+            <React.Fragment>
+              <Link to="events">Events</Link>
+              <Route path="/events" component={EventsList} />
+              <button className="appBtn" onClick={this.logout}>
+                Log Out
+              </button>
+            </React.Fragment>
+          ) : (
+            ''
+          )}
         </header>
-        {this.state.loggedIn == true ? <UserProfile loggedIn = {this.state.loggedIn}  /> : <Login className="login"></Login>}   
+        {this.state.loggedIn == true ? (
+          <UserProfile loggedIn={this.state.loggedIn} />
+        ) : (
+          <Login className="login" />
+        )}
       </div>
     );
   }
@@ -44,10 +50,11 @@ class App extends Component {
   logout(event) {
     event.preventDefault();
     axios({url: '/logout', method: 'get'})
-    .then((res)=>{this.setState({ loggedIn: res.loggedIn })})
-    .then( err => console.error(err)); 
+      .then(res => {
+        this.setState({loggedIn: res.loggedIn});
+      })
+      .then(err => console.error(err));
   }
 }
-
 
 export default App;
