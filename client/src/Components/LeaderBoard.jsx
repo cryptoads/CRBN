51d1a4d016761648f3aa744f07008f43ecf9fa7b
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-15 21:31:08 -0400

    pushing

--- client/src/Components/LeaderBoard.jsx
+++ client/src/Components/LeaderBoard.jsx
@@ -17,19 +17,19 @@ class LeaderBoard extends Component{
     }
 
     render(){
+        const imgStyle = {height: '100px'};
         const sorted = this.state.res.data.sort((a,b)=>{return a.score-b.score})
-        const cards = sorted.map((el, i)=>{return <tr><th key={i} scope="row">{i+1}</th>
-                                                  <td><img src={el.imgUrl} /></td>
-                                                  <td>{el.username}</td> 
-                                                  <td>{el.score}</td> </tr>})
+        const cards = sorted.map((el, i)=>{return <tr key={i}>
+                                                <th scope="row">{i+1}</th>
+                                                <td><img src={el.imgUrl} alt={el.username} style={imgStyle} />{el.username}</td> 
+                                                <td>{el.score}</td> </tr>})
         return(
             <div className="container">
             
-            <table class="table table-hover">
+            <table className="table table-hover">
               <thead>
                 <tr>
                   <th scope="col">#</th>
-                  <th scope="col"></th>
                   <th scope="col">User</th>
                   <th scope="col">CRBN Score</th>
                 </tr>
--- client/src/LoginImg.js
+++ client/src/LoginImg.js
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import Background from './LoginImg2.jpg';
 
 const LoginImg = {
