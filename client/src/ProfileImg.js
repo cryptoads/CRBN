62b0538a61dcb74b7dc234f281a007c382d0748a
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-19 10:11:07 -0400

    fix profile img

--- client/src/ProfileImg.js
+++ client/src/ProfileImg.js
@@ -3,7 +3,7 @@ import Background from './ProfileImg.jpg';
 
 const ProfileImg = {
     // width: '100vw',
-    // height: '100vh',
+    height: '100vh',
     backgroundImage: `url(${Background})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
