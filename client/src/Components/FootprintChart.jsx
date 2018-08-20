ee5e8ac2f274556bd0916b7ea268e3a5e0e12b5b
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-19 21:19:05 -0400

    fix

--- client/src/Components/FootprintChart.jsx
+++ client/src/Components/FootprintChart.jsx
@@ -35,9 +35,7 @@ class FootprintChart extends Component {
     )
   }
 
-  componentWillMount(){
 
-  }
 
   editLinkClick() {
     let modal = document.querySelector('#modal');
--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker, { unregister } from './registerServiceWorker';
 import { BrowserRouter as Routes } from 'react-router-dom';
+import { StaticRouter } from 'react-router'
 
 
 
-ReactDOM.render(<Routes><App /></Routes>, document.getElementById('root'));
+ReactDOM.render(<StaticRouter location={req.url}>
+<Routes><App /></Routes></StaticRouter>, document.getElementById('root'));
 unregister()
