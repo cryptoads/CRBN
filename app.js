0b6aa0565517671eede6763657497b8431c3c660
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-19 21:46:08 -0400

    fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
 
 setupAuth(app);
 
-app.use('/', indexRouter);
+app.use('/*', indexRouter);
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
