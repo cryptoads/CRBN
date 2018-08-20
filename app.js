4690efd7cc87358ab1206dfb01bbd533c9f1da39
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-20 19:12:40 -0400

    fix

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // dirname is public for local and client/build for prduction, add to .env
 app.use(express.static(path.join(__dirname, process.env.build)));
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
