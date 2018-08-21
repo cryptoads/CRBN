80a045471aaa40562dbc87f0ff305a4b49ce6cd0
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-20 20:33:20 -0400

    heroku build

--- models/index.js
+++ models/index.js
@@ -6,23 +6,23 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 // include config for local/ comment out for prod
-var config    = require(__dirname + '/../config/config.json')[env];
+// var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 var dotenv = require('dotenv');
 
 dotenv.load();
 
 // uncomment this stuff for production
-// var sequelize = new Sequelize(process.env.DATABASE_URL);
-
+var sequelize = new Sequelize(process.env.DATABASE_URL);
+// 
 // if(process.env.DATABASE_URL){
 // comment this stuff for local 
  
-  if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+//   if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
