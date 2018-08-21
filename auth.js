005377fcd610f8d0cb3c6381783acead62c81785
Chris Michels <chrisjohnmichels@gmail.com>
2018-08-20 21:47:45 -0400

    fb

--- auth.js
+++ auth.js
@@ -179,7 +179,7 @@ const setupAuth = (app) => {
         });
 
 
-    app.get('/auth/facbook', passport.authenticate('facebook'));
+    app.get('/auth/facebook', passport.authenticate('facebook'));
     app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/facebook/login' }),
     (req, res) => {
